8b1a097cac81f76ed6f35a0a1861be2e0cc73761
He Xian <he.xian@163.com>
2017-02-20 23:46:11 +0800

    Ensure thread safety

--- LagfreeServices/Lagfree.cs
+++ LagfreeServices/Lagfree.cs
@@ -17,61 +17,68 @@ namespace LagfreeServices
         internal const string CpuRestrainedCounterName = "CPU Restrained Process Count";
         internal const string IoRestrainedCounterName = "IO Restrained Process Count";
 
+        private static object PerfCounterSyncLock = new object();
         internal static bool SetupCategory()
         {
-            if (!PerformanceCounterCategory.Exists(CounterCategoryName))
+            lock (PerfCounterSyncLock)
             {
-
-                CounterCreationDataCollection counterDataCollection = new CounterCreationDataCollection();
-
-                // Add the counters.
-                CounterCreationData ioRestrainCount64 = new CounterCreationData()
+                if (!PerformanceCounterCategory.Exists(CounterCategoryName))
                 {
-                    CounterType = PerformanceCounterType.NumberOfItems64,
-                    CounterName = IoRestrainedCounterName,
-                    CounterHelp = "指示Lagfree HDD服务正限制的进程数量"
-                };
-                counterDataCollection.Add(ioRestrainCount64);
+
+                    CounterCreationDataCollection counterDataCollection = new CounterCreationDataCollection();
+
+                    // Add the counters.
+                    CounterCreationData ioRestrainCount64 = new CounterCreationData()
+                    {
+                        CounterType = PerformanceCounterType.NumberOfItems64,
+                        CounterName = IoRestrainedCounterName,
+                        CounterHelp = "指示Lagfree HDD服务正限制的进程数量"
+                    };
+                    counterDataCollection.Add(ioRestrainCount64);
 
 
-                CounterCreationData cpuRestrainCount64 = new CounterCreationData()
-                {
-                    CounterType = PerformanceCounterType.NumberOfItems64,
-                    CounterName = CpuRestrainedCounterName,
-                    CounterHelp = "指示Lagfree CPU服务正限制的进程数量"
-                };
-                counterDataCollection.Add(cpuRestrainCount64);
+                    CounterCreationData cpuRestrainCount64 = new CounterCreationData()
+                    {
+                        CounterType = PerformanceCounterType.NumberOfItems64,
+                        CounterName = CpuRestrainedCounterName,
+                        CounterHelp = "指示Lagfree CPU服务正限制的进程数量"
+                    };
+                    counterDataCollection.Add(cpuRestrainCount64);
 
-                // Create the category.
-                PerformanceCounterCategory.Create(CounterCategoryName,
-                    "指示Lagfree Services工作状态的性能指标。",
-                    PerformanceCounterCategoryType.SingleInstance, counterDataCollection);
+                    // Create the category.
+                    PerformanceCounterCategory.Create(CounterCategoryName,
+                        "指示Lagfree Services工作状态的性能指标。",
+                        PerformanceCounterCategoryType.SingleInstance, counterDataCollection);
 
-                return true;
+                    return true;
+                }
+                else return false;
             }
-            else return false;
         }
 
         private static IpcClientChannel AgentChannel;
         private static VisiblePids ForegroundPids = null;
         private static DateTime CacheAlive = DateTime.FromBinary(0);
         private static HashSet<int> VisiblePidsCache;
-        private static object InitSyncLock = new object();
+        private static object IpcSyncLock = new object();
         internal static HashSet<int> GetForegroundPids()
         {
-            lock (InitSyncLock) if (ForegroundPids == null) InitializeIpc();
-            DateTime Call = DateTime.UtcNow;
-            if (Call > CacheAlive)
+            lock (IpcSyncLock)
             {
-                try { VisiblePidsCache = ForegroundPids.Get(); }
-                catch
+                if (ForegroundPids == null) InitializeIpc();
+                DateTime Call = DateTime.UtcNow;
+                if (Call > CacheAlive)
                 {
-                    AgentPidCache = -1;
-                    VisiblePidsCache = new HashSet<int>();
+                    try { VisiblePidsCache = ForegroundPids.Get(); }
+                    catch
+                    {
+                        AgentPidCache = -1;
+                        VisiblePidsCache = new HashSet<int>();
+                    }
                 }
+                CacheAlive = Call.AddSeconds(2);
+                return VisiblePidsCache;
             }
-            CacheAlive = Call.AddSeconds(2);
-            return VisiblePidsCache;
         }
 
         private static int AgentPidCache = -1;
@@ -79,11 +86,14 @@ namespace LagfreeServices
         {
             get
             {
-                lock (InitSyncLock) if (ForegroundPids == null) InitializeIpc();
-                if (AgentPidCache == -1)
-                    try { AgentPidCache = ForegroundPids.GetPid(); }
-                    catch { }
-                return AgentPidCache;
+                lock (IpcSyncLock)
+                {
+                    if (ForegroundPids == null) InitializeIpc();
+                    if (AgentPidCache == -1)
+                        try { AgentPidCache = ForegroundPids.GetPid(); }
+                        catch { }
+                    return AgentPidCache;
+                }
             }
         }
 
